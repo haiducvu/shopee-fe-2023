e1bf89c0580b5a7c6a89debb48ab62419d9595c1
Hai Duc Vu <duchaivu1997@gmail.com>
2023-03-12 15:04:22 +0700

    [SETUP PROJECT]: Import path & Enable source map CSS

--- ShoppingOnline/src/App.tsx
+++ ShoppingOnline/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
+    <div className='App bg-green-500'>
       <div>
         <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src='/vite.svg' className='logo' alt='Vite logo' />
