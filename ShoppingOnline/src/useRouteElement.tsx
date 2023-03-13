d04b3758a6e7476d84214537313334478e2aed1f
Hai Duc Vu <duchaivu1997@gmail.com>
2023-03-13 23:35:56 +0700

    [SETUP ROUTER]: react-router-dom

---
+++ ShoppingOnline/src/useRouteElement.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from './layouts/RegisterLayout'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+
+export default function useRouteElement() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: <Register />
+    }
+  ])
+  return routeElements
+}
