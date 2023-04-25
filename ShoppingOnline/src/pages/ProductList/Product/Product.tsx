2d1830d71ce491788c45b28b242b398f75ef08be
Hai Vu <duchaivu1997@gmail.com>
2023-04-26 00:21:57 +0700

    [Algorithm]: Sort Product List Page

--- ShoppingOnline/src/pages/ProductList/Product/Product.tsx
+++ ShoppingOnline/src/pages/ProductList/Product/Product.tsx
@@ -11,33 +11,33 @@ interface Props {
 export default function Product({ product }: Props) {
   return (
     <Link to='/'>
-      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
-        <div className='w-full pt-[100%] relative'>
+      <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md'>
+        <div className='relative w-full pt-[100%]'>
           <img
             src={product.image}
             alt={product.name}
-            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+            className='absolute top-0 left-0 h-full w-full bg-white object-cover'
           />
         </div>
-        <div className='p-2 overflow-hidden'>
-          <div className='min-h-[2rem] line-clamp-2 text-xs'>
+        <div className='overflow-hidden p-2'>
+          <div className='min-h-[2rem] text-xs line-clamp-2'>
             [HÀNG HIỆU] Thắt Lưng Da Nam Khóa Tự Động Cao Cấp Dây Nịt Nam Mặt Xoay Chính Hãng , Phong Cách Hàn Quốc -
             v77men
           </div>
-          <div className='flex items-center mt-3'>
-            <div className='line-through max-w-[50%] text-gray-500 truncate'>
+          <div className='mt-3 flex items-center'>
+            <div className='max-w-[50%] truncate text-gray-500 line-through'>
               <span className='text-xs'>₫</span>
-              <span>{formatCurrency(product.price_before_discount)}</span>
+              <span className='text-sm'>{formatCurrency(product.price_before_discount)}</span>
             </div>
-            <div className='text-orange truncate ml-1'>
+            <div className='ml-1 truncate text-orange'>
               <span className='text-xs'>₫</span>
-              <span>{formatCurrency(product.price)}</span>
+              <span className='text-sm'>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
             <ProductRating rating={product.rating}></ProductRating>
             <div className='ml-2 text-sm'>
-              <span>{formatNumberToSocialStyle(product.sold)}</span>
+              <span className='text-sm'>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
