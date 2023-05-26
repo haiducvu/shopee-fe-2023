a17034bda8675bb829019db117c75e05f6dff3a2
Hai Vu <duchaivu1997@gmail.com>
2023-05-26 20:55:25 +0700

    [UI]: Quantity Controller

--- ShoppingOnline/src/components/InputNumber/index.ts
+++ ShoppingOnline/src/components/InputNumber/index.ts
@@ -1,3 +1,5 @@
 import InputNumber from './InputNumber'
 
 export default InputNumber
+// eslint-disable-next-line prettier/prettier
+export * from './InputNumber'
---
+++ ShoppingOnline/src/components/QuantityController/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line prettier/prettier
+import QuantityController from "./QuantityController";
+
+
+export default QuantityController
