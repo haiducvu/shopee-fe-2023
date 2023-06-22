0750f9f03a6c2498092e2a2749f1c8c1c80d96a6
Hai Vu <duchaivu1997@gmail.com>
2023-06-23 00:29:42 +0700

    [UI]: Cart Layout & custom hook User search Products

--- ShoppingOnline/src/useRouteElement.tsx
+++ ShoppingOnline/src/useRouteElement.tsx
@@ -10,6 +10,7 @@ import { AppContext } from './components/contexts/app.context'
 import path from './constants/path'
 import ProductDetail from './pages/ProductDetail'
 import Cart from './pages/Cart'
+import CartLayout from './pages/CartLayout'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -60,9 +61,9 @@ export default function useRouteElement() {
         {
           path: path.cart,
           element: (
-            <MainLayout>
+            <CartLayout>
               <Cart />
-            </MainLayout>
+            </CartLayout>
           )
         }
       ]
