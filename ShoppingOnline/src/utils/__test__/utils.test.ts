44a6cd9c95c1b9f0028f6ebf8ccc94750f0c212a
Hai Vu <duchaivu1997@gmail.com>
2023-08-07 13:01:27 +0700

    Unit test function

---
+++ ShoppingOnline/src/utils/__test__/utils.test.ts
@@ -0,0 +1,33 @@
+import { AxiosError } from 'axios'
+import { isAxiosError, isAxiosUnprocessableEntityError } from '../utils'
+import { describe, it, expect } from 'vitest'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+
+describe('isAxiosError', () => {
+  it('isAxiosError return boolean', () => {
+    expect(isAxiosError(new Error())).toBe(false)
+    expect(isAxiosError(new AxiosError())).toBe(true)
+  })
+})
+
+describe('isAxiosUnprocessableEntityError', () => {
+  it('isAxiosUnprocessableEntityError return boolean', () => {
+    expect(isAxiosUnprocessableEntityError(new Error())).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.InternalServerError,
+          data: null
+        } as any)
+      )
+    ).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.UnprocessableEntity,
+          data: null
+        } as any)
+      )
+    ).toBe(true)
+  })
+})
