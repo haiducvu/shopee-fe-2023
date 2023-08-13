f117fe87c42069eb0b4e47aafa6875bace9708b0
Hai Vu <duchaivu1997@gmail.com>
2023-08-13 11:17:08 +0700

    [UnitTest] Required React Hook in Login Page

---
+++ ShoppingOnline/src/pages/Login/Login.test.tsx
@@ -0,0 +1,20 @@
+import { screen, waitFor } from '@testing-library/react'
+import path from 'src/constants/path'
+import { logScreen, renderWithRouter } from 'src/utils/testUtils'
+import { describe, expect, it } from 'vitest'
+import matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers)
+
+describe('Login', () => {
+  it('Hiển thị lỗi required khi không nhập gì', async () => {
+    const { user } = renderWithRouter({ route: path.login })
+    await waitFor(() => {
+      expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
+    })
+    const submitButton = document.querySelector('form button[type="submit"]') as Element
+    user.click(submitButton)
+    expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
+    expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
+    // await logScreen()
+  })
+})
