be4d4ae518aab7fca22c15788e85e74ce75c0fe2
Hai Vu <duchaivu1997@gmail.com>
2023-08-14 19:58:51 +0700

    [UnitTest]: Input incorrect data on Form

--- ShoppingOnline/src/pages/Login/Login.test.tsx
+++ ShoppingOnline/src/pages/Login/Login.test.tsx
@@ -1,4 +1,4 @@
-import { screen, waitFor } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 import path from 'src/constants/path'
 import { logScreen, renderWithRouter } from 'src/utils/testUtils'
 import { describe, expect, it } from 'vitest'
@@ -6,15 +6,45 @@ import matchers from '@testing-library/jest-dom/matchers'
 expect.extend(matchers)
 
 describe('Login', () => {
+  beforeAll(async () => {
+    renderWithRouter({ route: path.login })
+    await waitFor(() => {
+      expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
+    })
+  })
   it('Hiển thị lỗi required khi không nhập gì', async () => {
     const { user } = renderWithRouter({ route: path.login })
     await waitFor(() => {
       expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
     })
     const submitButton = document.querySelector('form button[type="submit"]') as Element
-    user.click(submitButton)
-    expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
-    expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
-    // await logScreen()
+    // user.click(submitButton)
+    // expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
+    // expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
+    fireEvent.submit(submitButton)
+    await waitFor(async () => {
+      expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
+      expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
+    })
+
+    it('Hiển thị lỗi required khi không nhập gì', async () => {
+      const emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
+      const passwordInput = document.querySelector('form input[type="password"]') as HTMLElement
+      const submitButton = document.querySelector('form button[type="submit"]') as HTMLElement
+      fireEvent.change(emailInput, {
+        target: {
+          value: 'test@gmail'
+        }
+      })
+      fireEvent.change(passwordInput, {
+        target: {
+          value: '123'
+        }
+      })
+      fireEvent.submit(submitButton)
+      expect(await screen.findByText('Email khong dung dinh dang')).toBeTruthy()
+      expect(await screen.findByText('Do dai tu 6 - 160 ky tu')).toBeTruthy()
+      // await logScreen()
+    })
   })
 })
