78b55bb4e88230b26ee6a45bb8b7cf4528eff5f9
Hai Vu <duchaivu1997@gmail.com>
2023-08-15 21:25:46 +0700

    [UnitTest]: Using MSW to test React Query

--- ShoppingOnline/src/pages/Login/Login.test.tsx
+++ ShoppingOnline/src/pages/Login/Login.test.tsx
@@ -1,50 +1,70 @@
-import { fireEvent, screen, waitFor } from '@testing-library/react'
+import { screen, waitFor, fireEvent } from '@testing-library/react'
 import path from 'src/constants/path'
 import { logScreen, renderWithRouter } from 'src/utils/testUtils'
-import { describe, expect, it } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 import matchers from '@testing-library/jest-dom/matchers'
 expect.extend(matchers)
 
 describe('Login', () => {
+  let emailInput: HTMLInputElement
+  let passwordInput: HTMLInputElement
+  let submitButton: HTMLButtonElement
   beforeAll(async () => {
     renderWithRouter({ route: path.login })
     await waitFor(() => {
       expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
     })
+    emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
+    passwordInput = document.querySelector('form input[type="password"]') as HTMLInputElement
+    submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
   })
   it('Hiển thị lỗi required khi không nhập gì', async () => {
-    const { user } = renderWithRouter({ route: path.login })
-    await waitFor(() => {
-      expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
-    })
-    const submitButton = document.querySelector('form button[type="submit"]') as Element
-    // user.click(submitButton)
-    // expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
-    // expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
     fireEvent.submit(submitButton)
-    await waitFor(async () => {
-      expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
-      expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
+    await waitFor(() => {
+      expect(screen.queryByText('Email là bắt buộc')).toBeTruthy()
+      expect(screen.queryByText('Password là bắt buộc')).toBeTruthy()
     })
+  })
+  it('Hiển thị lỗi khi nhập value input sai', async () => {
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'test@mail'
+      }
+    })
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '123'
+      }
+    })
+    fireEvent.submit(submitButton)
+    await waitFor(() => {
+      expect(screen.queryByText('Email không đúng định dạng')).toBeTruthy()
+      expect(screen.queryByText('Độ dài từ 6 - 160 ký tự')).toBeTruthy()
+    })
+  })
 
-    it('Hiển thị lỗi required khi không nhập gì', async () => {
-      const emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
-      const passwordInput = document.querySelector('form input[type="password"]') as HTMLElement
-      const submitButton = document.querySelector('form button[type="submit"]') as HTMLElement
-      fireEvent.change(emailInput, {
-        target: {
-          value: 'test@gmail'
-        }
-      })
-      fireEvent.change(passwordInput, {
-        target: {
-          value: '123'
-        }
-      })
-      fireEvent.submit(submitButton)
-      expect(await screen.findByText('Email khong dung dinh dang')).toBeTruthy()
-      expect(await screen.findByText('Do dai tu 6 - 160 ky tu')).toBeTruthy()
-      // await logScreen()
+  it('Không nên hiển thị lỗi khi nhập lại value đúng', async () => {
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'test@mail.com'
+      }
     })
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '123456'
+      }
+    })
+    // Những trường hợp chứng minh rằng tìm không ra text hay là element
+    // Thì nên dùng query hơn là find hay get
+    await waitFor(() => {
+      expect(screen.queryByText('Email không đúng định dạng')).toBeFalsy()
+      expect(screen.queryByText('Độ dài từ 6 - 160 ký tự')).toBeFalsy()
+    })
+    fireEvent.submit(submitButton)
+    await logScreen()
+    await waitFor(() => {
+      expect(document.querySelector('title')?.textContent).toBe('Trang chủ | Shopee Clone')
+    })
+    // console.log(await screen.findByText('Email không đúng định dạng'))
   })
 })
--- ShoppingOnline/src/utils/testUtils.tsx
+++ ShoppingOnline/src/utils/testUtils.tsx
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react'
 import { expect } from 'vitest'
 import App from 'src/App'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const delay = (time: number) => {
   new Promise((resolve) => {
@@ -30,10 +31,40 @@ export const logScreen = async (
   screen.debug(body, 99999999)
 }
 
+const createWrapper = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      },
+      mutations: {
+        retry: true
+      }
+    },
+    logger: {
+      log: console.log,
+      warn: console.log,
+      // no more errors on the console
+      error: () => null
+    }
+  })
+  const Provider = ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+  return Provider
+}
+
+const Provider = createWrapper()
+
 export const renderWithRouter = ({ route = '/' } = {}) => {
   window.history.pushState({}, 'Test Page', route)
   return {
     user: userEvent.setup(),
-    ...render(<App />, { wrapper: BrowserRouter })
+    ...render(
+      <Provider>
+        <App />
+      </Provider>,
+      { wrapper: BrowserRouter }
+    )
   }
 }
